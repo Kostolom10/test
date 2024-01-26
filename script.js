f66e731cdc7a2d8194301ab4ec55832987b03bb6
Kostolom10 <84643403+Kostolom10@users.noreply.github.com>
2024-01-26 15:48:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// let number = 0;
+// while (number > 0) {
+//   console.log(number--);
+// }
+// do {
+//   console.log(number);
+// } while (number > 0);
+// while (true) {
+//   alert("Привет");
+//   let i = prompt("Остановаить цикл?", "");
+//   if (i == "yes") {
+//     break;
+//   }
+// }
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+// let name = "Dima";
+// let names = ["Dima", "Vova", "Olya"];
+// console.log(names);
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
